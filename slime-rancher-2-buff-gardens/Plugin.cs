2141c3e7f09ddc17dfeedb7713bf4d4cb8cf8aa1
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-10-19 01:16:58 +0100

    initial commit: almost completely 'clean' plugin

---
+++ slime-rancher-2-buff-gardens/Plugin.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+
+namespace BuffGardens;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    public override void Load()
+    {
+        
+    }
+}
